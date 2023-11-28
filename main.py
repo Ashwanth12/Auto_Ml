edcd41c009d3415545d36241a0f6e27b3b2138b5
Ashwanth12 <48740107+Ashwanth12@users.noreply.github.com>
2023-11-28 08:32:20 +0530

    Update main.py
    version 3.7

--- main.py
+++ main.py
@@ -1019,7 +1019,7 @@ if choice == "Download":
                     data=data,
                     file_name="./" + i + ".pkl"
                 )
-        if st.toggle("Do Prediction"):
+        if st.button("Do Prediction",type="primary"):
             if "chosen_target" in st.session_state:
                 chosen_target = st.session_state['chosen_target']
                 cols = df.columns
@@ -1068,7 +1068,7 @@ if choice == "Download":
                     data=data,
                     file_name="./" + i + ".pkl"
                 )
-        if st.toggle("Do Classification"):
+        if st.button("Do Classification",type="primary"):
             if "chosen_target" in st.session_state:
                 chosen_target = st.session_state['chosen_target']
                 cols = df.columns
